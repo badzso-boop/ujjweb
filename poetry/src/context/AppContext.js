ad7516eb25b40a9e9c322d1c907fc71d511ee373
badzso-boop <norbert.ujj@gmail.com>
2024-06-04 11:28:12 +0200

    update

--- poetry/src/context/AppContext.js
+++ poetry/src/context/AppContext.js
@@ -20,9 +20,8 @@ export const AppProvider = ({ children }) => {
     try {
       const apiUrl = process.env.REACT_APP_API_URL;
       
-      const response = await axios.get(`${apiUrl}/projects`, { withCredentials: true });
-      console.log(response)
-      // setPoems(response.data);
+      const response = await axios.get(`${apiUrl}/poems`, { withCredentials: true });
+      setPoems(response.data);
     } catch (error) {
       console.error('Error fetching poems:', error.message);
     }
